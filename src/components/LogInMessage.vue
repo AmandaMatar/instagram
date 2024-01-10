46e4e47c185cfebc048b3270780f4b9c885739f3
Amanda Matar <amandamatar@gmail.com>
2024-01-10 18:11:14 -0300

    projeto instagram código

---
+++ src/components/LogInMessage.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+    <div class="timeline-container">
+        <h2>Log in to see posts</h2>
+    </div>
+</template>
---
+++ src/components/UploadoPhotoModal.vue
@@ -0,0 +1,91 @@
+<script setup>
+  import { ref, defineProps } from 'vue';
+  import {supabase} from "../supabase"
+  import {useUserStore} from "../stores/users"
+  import { storeToRefs } from 'pinia';
+
+  const userStore = useUserStore()
+
+  const {user} = storeToRefs(userStore);
+  const props = defineProps(['addNewPost'])
+
+  const loading = ref(false)
+  const errorMessage = ref("")
+  const open = ref(false);
+  const caption = ref("")
+  const file = ref(null)
+  
+  const showModal = () => {
+    open.value = true;
+  };
+  
+  const handleOk = async () => {;
+    loading.value = true;
+    const fileName = Math.floor(Math.random() *100000000000000000000)
+    let filePath
+    if(file.value){
+      const {data, error} = await supabase.storage.from("images").upload('public/' + fileName, file.value);
+
+      if(error){
+        loading.value = false
+        return errorMessage.value = "Unable to apload image"
+      }
+      
+      filePath = data.path
+      await supabase.from("posts").insert({
+        url: filePath,
+        caption: caption.value,
+        owner_id: user.value.id
+      })
+
+    }
+
+    loading.value = false;
+    open.value = false;
+    caption.value = "";
+    props.addNewPost({
+      url: filePath,
+      caption: caption.value,
+      owner_id: user.value.id
+    })
+  };
+
+  const handleUploadChange = (e) => {
+    if(e.target.files[0]){
+      file.value = e.target.files[0]
+    }
+  }
+</script>
+
+<template>
+    <div>
+      <AButton @click="showModal">Upload Photo</AButton>
+      <AModal v-model:open="open" title="Upload Photo" @ok="handleOk">
+        <div v-if="!loading">
+          <input type="file" accept=".jpeg,.png" @change="handleUploadChange">
+          <AInput 
+            v-model:value="caption" 
+            placeholder="Caption..."
+            :maxLength="50"
+          />
+          <ATypographyText v-if="errorMessage" type="danger">{{ errorMessage }}</ATypographyText>
+        </div>
+        <div class="spinner" v-else>
+          <ASpin />
+        </div>
+      </AModal>
+    </div>
+</template>
+
+<style scoped>
+  input {
+    margin-top: 10px;
+  }
+
+  .spinner {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+  
